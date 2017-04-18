93c40765bf741e803730969106baf7f9cdaea3e3
Drew Hornbein <drew@dru2.com>
2017-04-18 18:18:27 -0400

    Added action network

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,3 +1,10 @@
 //= require jquery
 //= require tether
 //= require bootstrap-sprockets
+
+$(document).ready(function() {
+    $(document).on('can_embed_loaded', function(e) {
+        // do your work here, after the widget is loaded
+        console.log(e);
+    });
+});
