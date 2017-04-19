15a39fbd0ab784709d401c2ee401130cd3524a13
Drew Hornbein <drew@dru2.com>
2017-04-19 12:59:26 -0400

    Letter writing swap text function

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,8 +3,20 @@
 //= require bootstrap-sprockets
 
 $(document).ready(function() {
+  var formID        = "#embedForm",
+      newFormID     = "#new_delivery",
+      letterFormID  = "#letter-form";
+
     $(document).on('can_embed_loaded', function(e) {
         // do your work here, after the widget is loaded
-        console.log(e);
+        var submitText = $(formID).data('submit');
+        var submitBtn = $(e.target).find(letterFormID + " input[type='submit']");
+
+        submitBtn.each( function(i,e){
+          if (typeof submitText !== undefined) {
+            $(e).attr("data-original",$(e).val());
+            $(e).val(submitText);
+          }
+        });
     });
 });
