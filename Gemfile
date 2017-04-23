9472106f0e8aacf8b7c92b67bd4cdb27b1613e88
James Vaughan <jamesvaughan@Jamess-MacBook-Pro.local>
2017-04-23 05:04:29 -0400

    Added in middleman dato and started index page localization.

--- Gemfile
+++ Gemfile
@@ -5,10 +5,12 @@ gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'middleman-sprockets'
 gem 'jquery-middleman'
+gem "middleman-dato"
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'rake'
+gem 'pry'
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- config.rb
+++ config.rb
@@ -1,33 +1,14 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
 activate :sprockets
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
 
 # Helpers
 # Methods defined in the helpers block are available in templates
@@ -39,6 +20,10 @@ page '/*.txt', layout: false
 #   end
 # end
 
+def slugify(title)
+	title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+end
+
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
@@ -47,3 +32,39 @@ configure :build do
   activate :minify_javascript
   set :http_prefix, '/backers-of-hate-static'
 end
+
+activate :dato
+
+activate :i18n, langs: dato.available_locales
+set :lang, :en
+
+dato.tap do |dato|
+  # iterate over all the administrative area languages
+  dato.available_locales.each do |locale|
+
+    # switch to the nth locale
+    I18n.with_locale(locale) do
+
+    	proxy(
+    		"#{locale}/index.html",
+    		"/templates/index.html",
+    		locals: { locale: locale },
+    		locale: locale
+    	)
+
+      # iterate over the "Article" records...
+      dato.organizations.each do |org|
+        # ...and create a proxy file for each article
+        proxy(
+          "/#{locale}/#{org.url_title}.html",
+          "/templates/card.html",
+          locals: { organization: org },
+          locale: locale
+        )
+      end
+    end
+  end
+end
+
+ignore "templates/index.html.erb"
+ignore "templates/card.html.erb"
